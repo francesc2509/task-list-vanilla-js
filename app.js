729eee077f3e0b5f59918147a36a1b435ffc0f93
Francesc Manarel Bueno <francesc2509@outlook.com>
2021-09-26 13:06:10 +0200

    Add clearAll and filter functionalities

--- app.js
+++ app.js
@@ -2,22 +2,39 @@ const mainDiv = document.getElementById('main');
 const taskForm = mainDiv.querySelector('#task-form');
 const newTaskInput = taskForm.task;
 const cardActionDiv = mainDiv.querySelector(".card-action");
-const taskFilterInput = cardActionDiv.querySelector('.task-filter');
+const taskFilterInput = cardActionDiv.querySelector('#task-filter');
 const taskListUl = cardActionDiv.querySelector(".collection");
-const clearTaskAnchor = cardActionDiv.querySelector(".clear-tasks");
+const clearTasksAnchor = cardActionDiv.querySelector(".clear-tasks");
 
-const removeTask = (deleteTaskAnchor) => {
-    const taskLi = deleteTaskAnchor.parentElement;
-    
+const removeTask = (taskLi, deleteTaskAnchor) => {
     deleteTaskAnchor.removeEventListener('click', deleteTask);
     taskListUl.removeChild(taskLi);
 };
 
 const deleteTask = (event) => {
-    removeTask(event.target.parentElement);
+    const deleteTaskAnchor = event.target.parentElement;
+    const taskLi = deleteTaskAnchor.parentElement;
+    if (!confirm(`The task ${taskLi.textContent} will be removed. Are you sure you want to continue?`)) {
+        return;
+    }
+
+    removeTask(taskLi, deleteTaskAnchor);
     
     event.preventDefault();
 };
+
+const clearTasks = (event) => {
+    if (!confirm('All tasks will be removed. Are you sure you want to continue?')) {
+        return;
+    }
+
+
+    for (let taskLi = taskListUl.firstElementChild; taskLi; taskLi = taskListUl.firstElementChild) {
+        removeTask(taskLi, taskLi.querySelector(".delete-item"));
+    }
+
+    event.preventDefault();
+}
 
 const addTask = (event) => {
     const deleteTaskIcon = document.createElement('i');
@@ -39,8 +56,25 @@ const addTask = (event) => {
     event.preventDefault();
 };
 
+const filterTasks = (event) => {
+    const filterText = taskFilterInput.value.toLocaleLowerCase();
+
+    for (let taskLi = taskListUl.firstElementChild; taskLi; taskLi = taskLi.nextElementSibling) {
+        const taskName = taskLi.textContent;
+        
+        let displayValue = 'none';
+        if (taskName.toLocaleLowerCase().includes(filterText)) {
+            displayValue = 'list-item';
+        }
+
+        taskLi.style.display = displayValue;
+    }
+};
+
 function loadEventListeners() {
     taskForm.addEventListener('submit', addTask);
+    clearTasksAnchor.addEventListener('click', clearTasks);
+    taskFilterInput.addEventListener('input', filterTasks);
 }
 
 loadEventListeners();

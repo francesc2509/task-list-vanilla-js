d3569db432ebdba5d143b47f6cd33ffd9b27be77
Francesc Manarel Bueno <francesc2509@outlook.com>
2021-09-26 14:04:04 +0200

    Add task 'persitence'

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const TASKS_KEY = 'tasks';
+
 const mainDiv = document.getElementById('main');
 const taskForm = mainDiv.querySelector('#task-form');
 const newTaskInput = taskForm.task;
@@ -6,9 +8,26 @@ const taskFilterInput = cardActionDiv.querySelector('#task-filter');
 const taskListUl = cardActionDiv.querySelector(".collection");
 const clearTasksAnchor = cardActionDiv.querySelector(".clear-tasks");
 
+let tasks = [];
+
+const getTasks = () => {
+    let tasks = JSON.parse(localStorage.getItem(TASKS_KEY));
+    if (!tasks || !(tasks instanceof Array)) {
+        tasks = [];
+    }
+
+    return tasks;
+};
+
 const removeTask = (taskLi, deleteTaskAnchor) => {
     deleteTaskAnchor.removeEventListener('click', deleteTask);
     taskListUl.removeChild(taskLi);
+};
+
+const saveTask = (name) => {
+    tasks.push(name);
+    console.log(name);
+    localStorage.setItem(TASKS_KEY, JSON.stringify(tasks));
 };
 
 const deleteTask = (event) => {
@@ -19,6 +38,12 @@ const deleteTask = (event) => {
     }
 
     removeTask(taskLi, deleteTaskAnchor);
+    tasks = tasks.filter(task => task !== taskLi.textContent)
+    if (!tasks || tasks.length === 0) {
+        localStorage.removeItem(TASKS_KEY);
+    } else {
+        localStorage.setItem(TASKS_KEY, JSON.stringify(tasks));
+    }
     
     event.preventDefault();
 };
@@ -32,11 +57,13 @@ const clearTasks = (event) => {
     for (let taskLi = taskListUl.firstElementChild; taskLi; taskLi = taskListUl.firstElementChild) {
         removeTask(taskLi, taskLi.querySelector(".delete-item"));
     }
+    tasks = [];
+    localStorage.removeItem(TASKS_KEY);
 
     event.preventDefault();
 }
 
-const addTask = (event) => {
+const createTask = (name) => {
     const deleteTaskIcon = document.createElement('i');
     deleteTaskIcon.classList.add('fa', 'fa-remove');
 
@@ -47,12 +74,26 @@ const addTask = (event) => {
 
     const newTaskLi = document.createElement('li');
     newTaskLi.classList.add('collection-item');
-    newTaskLi.appendChild(document.createTextNode(newTaskInput.value));
+    newTaskLi.appendChild(document.createTextNode(name));
     newTaskLi.appendChild(deleteTaskAnchor);
 
     taskListUl.appendChild(newTaskLi);
+};
 
-    newTaskInput.value = '';
+const addTask = (event) => {
+    const taskName = newTaskInput.value;
+
+    const index = tasks.findIndex(task => task === taskName);
+    if (index > -1) {
+        alert('Task already exists!');
+    } else {
+        createTask(taskName);
+        filterTasks(event);
+        saveTask(taskName);
+        
+        newTaskInput.value = '';
+    }
+    
     event.preventDefault();
 };
 
@@ -77,4 +118,8 @@ function loadEventListeners() {
     taskFilterInput.addEventListener('input', filterTasks);
 }
 
-loadEventListeners();
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadEventListeners();
+    tasks = getTasks();
+    tasks.forEach(task => createTask(task))
+});
